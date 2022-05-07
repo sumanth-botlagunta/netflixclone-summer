2625b4534bd79fa4b8d2f126599e8b561c885774
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-05-07 12:36:20 +0530

    added trailer pops as possible

--- src/components/home/Row.js
+++ src/components/home/Row.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ReactPlayer from "react-player";
+import movieTrailer from "movie-trailer";
 
 function Row({ title, fetchurl, number, IsLargeRow }) {
   const [movies, setMovies] = React.useState([]);
+  const [trailerUrl, setTrailerUrl] = React.useState("");
 
   const baseUrl = "https://api.themoviedb.org/3";
 
@@ -16,7 +19,29 @@ function Row({ title, fetchurl, number, IsLargeRow }) {
     fetchData();
   }, [fetchurl]);
 
-  // console.log("movies data for ", movies);
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    console.log("clicked movie >>>", movie);
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || '')
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+          console.log("trailer url >>>", urlParams.get('v'));
+          // setTrailerUrl(url);
+        })
+        .catch(err => console.log(err));
+    }
+  }
 
   return (
     <div className="row" key={number}>
@@ -27,15 +52,24 @@ function Row({ title, fetchurl, number, IsLargeRow }) {
           <>
             <img
               key={movie.id}
+              onClick={() => handleClick(movie)}
               className={`movieImage ${IsLargeRow && "large_row"}`}
-              src={`${imageUrl}${
-                IsLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
+              src={`${imageUrl}${IsLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
               alt={movie.title}
             />
           </>
         ))}
       </div>
+      {/* {trailerUrl && <ReactPlayer videoId={trailerUrl}/>} */}
+      {
+        trailerUrl && <iframe src={`https://www.youtube.com/embed/${trailerUrl}`}
+          frameborder='0'
+          className="trailer"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
+          
+        />
+      }
     </div>
   );
 }
